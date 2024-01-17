7d91392c4e1331d3685ac1df9a154b09376739b3
yuan <156386799+whyuan00@users.noreply.github.com>
2024-01-17 18:01:53 +0900

    Add files via upload

---
+++ 240118/1157_yuan.py
@@ -0,0 +1,16 @@
+Words = list(input().upper())
+
+dict = {}
+for i in Words:
+    if i in dict:
+        dict[i] +=1
+    else:
+        dict[i] = 1
+
+max_value = max(dict.values())
+max_keys = [key for key, value in dict.items() if value == max_value]
+
+if len(max_keys) >1:
+    print("?")
+else: 
+    print(max_keys[0])
