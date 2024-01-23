04e14288b92601e8970869ebf6cd327f966cc6f9
rlarbfla12 <156387260+rlarbfla12@users.noreply.github.com>
2024-01-23 21:16:35 +0900

    Update 2292_rlarbfla12.py

--- 240123/2292_rlarbfla12.py
+++ 240123/2292_rlarbfla12.py
@@ -4,8 +4,6 @@ room = 1   # 방 총 개수 (1, 7, 19, 37 ...)
 count = 1   # 각각의 둘레 (1: 1개, 2: 6개, 3: 12개 ...)
 
 while a > room:
-    if a == 1 :
-        break
     room = room + (6 * count) 
     count = count + 1
 print(count)
