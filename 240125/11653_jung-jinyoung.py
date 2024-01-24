f8254f523bda79b07a764ccbfb2982f8373ea31a
jinyoung_jung <ya_1001@naver.com>
2024-01-24 22:47:36 +0900

    240124 jung-jinyoung

---
+++ 240125/11653_jung-jinyoung.py
@@ -0,0 +1,15 @@
+N = int(input())
+
+my_int = 2 #최소 인수 
+
+while N >= my_int:
+    if N % my_int == 0: #나누어 떨어질 때
+        N = N // my_int
+        print(my_int)
+        continue
+    elif N == my_int: # 인수와 동일할 때 
+        print(N)
+    else :
+        my_int +=1 #더이상 나누어 지지 않을 때
+        continue
+    
