5144ccbe161014e4b5f9da70a37990e31613698e
unknown <ghwls1221@mail.hongik.ac.kr>
2024-01-28 03:18:57 +0900

    re-solved problem upload, HoseaKim

--- 240120/10798_hoseakim_(seroRead).py
+++ 240120/10798_hoseakim_(seroRead).py
@@ -1,18 +1,18 @@
 # 문자열 5줄 입력
 garo_list = [(list(input())) for _ in range(5)]
 
-# # 각 줄마다 길이 측정
+# 각 줄마다 길이 측정
 garo_len_list = [len(garo_list[i]) for i in range(5)]
 
-# # 0번줄보다 길이가 짧은 행에 공백 채워넣기
-for i in range(1,4):
-    if garo_len_list[i] < garo_len_list[0]:
-        for _ in range(garo_len_list[0] - garo_len_list[i]):
+# 0번줄보다 길이가 짧은 행에 공백 채워넣기
+for i in range(5):
+    if garo_len_list[i] < max(garo_len_list):
+        for _ in range(max(garo_len_list) - garo_len_list[i]):
             garo_list[i].append('')
 
 # 한 열씩 읽어 한 줄의 문자열로 출력
 sero = ''
-for j in range(garo_len_list[0]):
+for j in range(max(garo_len_list)):
     for i in range(5):
         sero += garo_list[i][j]
 print(sero)
--- 240127/15904_HoseaKim_UCPC.py
+++ 240127/15904_HoseaKim_UCPC.py
@@ -2,25 +2,33 @@ in_str = input()
 
 old_str = ''
 new_str = ''
+
 for i in in_str:
     if i == 'U' or i == 'C' or i == 'P':
         old_str += i
+
 if 'UCPC' in old_str:
-        print('I love UCPC')
+    print('I love UCPC')
+elif set('UCPC') != set(old_str):
+    print('I hate UCPC')
+
 else:
     pre = 0
-    index_1st_u = old_str.index('U')
+    try:
+        index_1st_u = old_str.index('U')
+    except ValueError:
+        print('I hate UCPC')
     for j in old_str[index_1st_u:]:
-        if pre == 0 and j == 'U':
-            new_str += j
-            pre = j
-        elif j == 'C' and (pre == 'U' or pre == 'P'):
-            new_str += j
-            pre = j
-        elif j == 'P' and pre == 'C':
-            new_str += j
-            pre = j
-        pre = None
+        if j == 'U' and pre != 0:
+            continue
+        elif j == 'C' and pre != 'U' and pre != 'P':
+            continue
+        elif j == 'P' and pre != 'C':
+            continue
+
+        new_str += j
+        pre = j
+
     if 'UCPC' in new_str:
         print('I love UCPC')
     else:
