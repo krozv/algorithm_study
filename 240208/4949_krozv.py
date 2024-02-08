0fb69d2f315b4d2177a52465d84cfda0286a906e
krozv <krozv.code@gmail.com>
2024-02-08 14:40:06 +0900

    modify file

--- 240208/4949_krozv.py
+++ 240208/4949_krozv.py
@@ -6,10 +6,10 @@ stack으로 풀 것임
 균형잡힌 문자열인지 판단
 """
 import sys
-sys.stdin = open('test.txt','r')
-input = sys.stdin.readline
+# sys.stdin = open('test.txt','r')
+# input = sys.stdin.readline
 while True:
-    string = input().rstrip('\n')
+    string = input()
     if string == '.':
         break
     # stack 생성
@@ -24,6 +24,10 @@ while True:
                     break
                 if [stack[-1], char] in [['[', ']'], ['(', ')']]:
                     stack.pop()
+                # 하기 코드 작성 안할 시 틀림
+                else:
+                    print('no')
+                    break
     else:
         if len(stack) == 0:
             print('yes')
