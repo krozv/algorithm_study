2cd65737d97d8d2c2f5203173762d8502cb7149b
krozv <krozv.code@gmail.com>
2024-02-10 00:09:45 +0900

    modify file

--- 240210/238_krozv.py
+++ 240210/238_krozv.py
@@ -63,24 +63,26 @@ class Solution:
 4th
 reduce module 사용이 최선인가? 의문.
 아 모르겠다...ing
+누적합을 사용해볼까? -> 통과 but 362 ms나 걸림 ㅠ
 """
-from functools import reduce
 class Solution:
     def productExceptSelf(self, nums: [int]) -> [int]:
         N = len(nums)
-        output = [0] * N
-        new_nums = []
-        for idx in range(N):
-            new_nums.append(nums.pop(0))
-            if len(nums) == 1:
-                output[idx] = nums[0]
-            else:
-                output[idx] *= reduce(lambda x, y: x * y, nums)
-                if len(new_nums) > 1:
-                    output[idx] *= reduce(lambda x, y: x * y, new_nums[:-1])
+        cnt = [0] * 61
+        output = [1] * N
+        for num in nums:
+            cnt[num + 30] += 1
+        for i in range(N):
+            for idx, num in enumerate(cnt):
+                if num > 0:
+                    # print(idx,  num)
+                    if idx-30 == nums[i]:
+                        output[i] *= (idx-30) ** (num-1)
+                    else:
+                        output[i] *= (idx-30) ** num
         return output
 
 
 c = Solution()
-input = [1, 2, 3]
+input = [1, 2]
 print(c.productExceptSelf(input))
