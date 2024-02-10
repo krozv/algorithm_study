295364b0331d613db0707fea639dc14996637ea6
krozv <krozv.code@gmail.com>
2024-02-10 10:27:34 +0900

    Update 121_Thoughtful-Lover.py
    modify

--- 240210/121_Thoughtful-Lover.py
+++ 240210/121_Thoughtful-Lover.py
@@ -4,18 +4,15 @@ def best_time_for_stock(price_list):
     profit = 0
     N = len(price_list)
 
-        for i in range(1, N):
+        for i in range(1, N-1):
             if max < price_list[i]:
                 max = price_list[i]
-            elif i < N-1:
-                if profit < max - min:
-                    profit = max - min
-                min = price_list[i]
-                max = price_list[i]
+                # ....? 알아서 잘 해보기..!
+            if min > price_list[i] # min값 구해야 함
+            
+            if profit < max - min:
+                profit = max - min
 
-            else:
-                if profit < max - min:
-                    profit = max - min
 
 
 prices = list(map(int, input().split()))       # 입력을 prices = [1, 2, 3, 4] 이런 식으로 받던데..?
