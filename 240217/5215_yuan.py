ba743a9f35ad27b356e94035bd28c38ab95ae714
yuan <whyuan@gmail.com>
2024-02-17 11:34:42 +0900

    yuan

--- 240217/5215_yuan.py
+++ 240217/5215_yuan.py
@@ -1,3 +1,4 @@
+'''
 T = int(input())
 for tc in range(1,T+1):
 
@@ -24,12 +25,52 @@ for tc in range(1,T+1):
                 mx_tas = sum_tas
 
     print(f'#{tc} {mx_tas}')
+'''
+
+#백트래킹 # 호준
+def f(i, cal_s, score): # 인덱스, 직전까지 칼로리합, 직전까지 점수 합
+    global max_score
+    if cal_s > max_score:
+        max_score = cal_s
+    if i==n:
+        return # n번까지 다하면 끝
+
+    else:
+        if cal_s + data[i][1] <= lmt: # 총칼로리보다 작을때 백트래킹
+            f(i+1, cal_s+data[i][1], score+data[i][0])
+        f(i+1,cal_s,score) # 클때 cal_에 포함안하고 다음꺼 data[i+1] 찾으러 감
 
 
-#dfs?
-# 재귀로 부분집합 구하기?
+n, lmt = 5, 1000 #총 재료수, 칼로리 한계
+data = [list() for _ in range(n)] # 여러줄 이차원 리스트로 받기
+max_score = 0
+f(0,0,max_score) # 시작인덱스0/칼로리누적0/max넣기
+print(max_score)
 
-#1. [tas, cal] 을 다 넣은 통 리스트 만들기
-#2. 통 리스트 중에 부분집합 다 탐색
-#3. 탐색의 [0] 인덱스를 다 더한값 sum_tas [1] 인덱스를 다 더한값 sum_cal
-# 4. if sum_tas >lmt and sum_cal > total_cal 일때 total_cal로 바꿈
+
+# 비트 + 백트래킹 # 주연
+# 전제 부분집합 구하기 , 점수와 칼로리 계산해서 점수는 높, 칼로리 낮
+def f(i,cal_s,max_score):
+    if cal_s > lmt: # 중간이라도 칼로리 넘치면 return(백트래킹)
+        return
+    if i == n: # 인덱스 끝까지 왔을떄
+        score = 0
+        for j in range(n):
+            score += data[j][0]  # 점수 총합 구하기
+        if score > max_score:
+            max_score = score
+
+    # 비트 만들기
+    else:
+        bit[i] = 0
+        f(i+1, cal_s, max_score)
+
+        bit[i] = 1
+        f(i+1, cal_s+data[i][1], max_score) # 백트래킹
+
+n, lmt = 5, 1000 #총 재료수, 칼로리 한계
+data = [list() for _ in range(n)] # 여러줄 이차원 리스트로 받기
+bit = [0] * N
+max_score =  0
+f(0,0,max_score) # 시작인덱스0/칼로리누적0/max넣기
+print(max_score)
