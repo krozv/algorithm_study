646263a84539e64c3c4cbc47016f1ab837c23d73
krozv <krozv.code@gmail.com>
2024-02-22 12:46:17 +0900

    backup krozv

--- 240224/5639_krozv(ing).py
+++ 240224/5639_krozv(ing).py
@@ -2,4 +2,14 @@
 """
 왼쪽: 최대힙
 오른쪽: 최소힙
+전위 순회한 결과를 후위 순회로 변환
 """
+import sys
+input = sys.stdin.readline
+num = []
+while True:
+    try:
+        num.append(int(input()))
+    except:
+        break
+print(num)
